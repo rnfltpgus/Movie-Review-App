1cd5a82108dc21c1c313b72afa022f80b24da55d
rnfltpgus <jpp901220@gmail.com>
2022-09-03 03:28:35 +0900

    ⚙️ Feat: GlobalStyles 적용, 폴더 분리, 초기 목데이터 추가

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import ReviewHistory from "../components/ReviewHistory";
+import GlobalStyles from "./GlobalStyles";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <ReviewHistory />
+    </>
+  );
+};
+
+export default App;
