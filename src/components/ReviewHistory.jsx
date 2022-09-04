6825ff470163632994080b751bc80c5efcead2a8
rnfltpgus <jpp901220@gmail.com>
2022-09-04 16:29:58 +0900

    ✅ Feat, Style: 등록 또는 일반상황에서 제목기준으로 정렬 추가, Local Storage 적용 및 애러체크, 코드스타일 정검

--- src/components/ReviewHistory.jsx
+++ src/components/ReviewHistory.jsx
@@ -6,22 +6,26 @@ import ReviewCade from "./ReviewCade";
 import styled from "styled-components";
 
 const ReviewHistory = () => {
-  const [reviews, setReviews] = useState();
+  const [reviews, setReviews] = useState(
+    JSON.parse(localStorage.getItem("review-data"))
+  );
 
   useEffect(() => {
     async function getReviewData() {
       try {
-        const response = await fetch("./data/data.json");
-        const data = await response.json();
+        if (!reviews) {
+          const response = await fetch("./data/data.json");
+          const data = await response.json();
 
-        setReviews(data);
+          setReviews(data);
+        }
       } catch (error) {
         console.error(error);
       }
     }
 
     getReviewData();
-  }, [setReviews]);
+  }, [reviews, setReviews]);
 
   const deleteReview = (id) => {
     setReviews(
@@ -35,14 +39,23 @@ const ReviewHistory = () => {
     setReviews([review, ...reviews]);
   };
 
+  localStorage.setItem("review-data", JSON.stringify(reviews));
+
   return (
     <ReviewHistoryContainer>
       <div className="review-history-title">신규 리뷰 등록</div>
       <CreateReview handleReviewCreate={addReview} />
+      <div className="review-history-title">리뷰 검색</div>
+
       <div className="review-history-title">리뷰 내역</div>
       <ul className="review-list">
         {reviews &&
           reviews
+            .sort((a, b) => {
+              if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
+              else if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
+              else return 0;
+            })
             .sort((a, b) => b.score - a.score)
             .map((review) => {
               const { id, title, comment, score } = review;
